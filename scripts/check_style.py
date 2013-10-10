3547ebce91f6f9c78054646236f44be935ded7f4
Gama <misept.dieseneuf@gmail.com>
2013-10-10 18:28:25 +0200

    SCRIPTS: Added license text + copyright to existing script
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- scripts/check_style.py
+++ scripts/check_style.py
@@ -2,6 +2,28 @@
 '''
 @file check_style.py
 @brief Various utility functions/classes for checking C++ code style
+@author gm
+@copyright gm 2013
+
+This file is part of OpenMini
+
+OpenMini is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenMini is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+'''
+Note that all of this mimics C++ code for testing/prototyping purpose.
+Hence it may not really seems "pythonic" and not intended to be in any way.
 '''
 
 import os
