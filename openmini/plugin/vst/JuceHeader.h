cb484326fc713e5be23fbc0ab08ad0486ca9291c
Gama <misept.dieseneuf@gmail.com>
2013-10-24 15:58:32 +0200

    PLUGIN: added missing include
    It was required for Juce Midi stuff.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- openmini/plugin/vst/JuceHeader.h
+++ openmini/plugin/vst/JuceHeader.h
@@ -16,6 +16,7 @@
 #include "AppConfig.h"
 #include "modules/juce_audio_basics/juce_audio_basics.h"
 #include "modules/juce_audio_devices/juce_audio_devices.h"
+#include "modules/juce_audio_utils/juce_audio_utils.h"
 #include "modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
 #include "modules/juce_audio_processors/juce_audio_processors.h"
 #include "modules/juce_core/juce_core.h"
