376372ed209b2c5fda73882aa3c22e0602014ec3
Gama <misept.dieseneuf@gmail.com>
2013-11-27 15:21:56 +0100

    SYNTHESIZER: Added a filter object
    This wraps the internal low pass filter.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ openmini/src/synthesizer/filter.cc
@@ -0,0 +1,78 @@
+/// @filename filter.cc
+/// @brief Filter: wrapper around an internal filter - implementation
+/// @author gm
+/// @copyright gm 2013
+///
+/// This file is part of OpenMini
+///
+/// OpenMini is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// OpenMini is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "openmini/src/synthesizer/filter.h"
+
+#include "openmini/src/filters/factory.h"
+#include "openmini/src/filters/filter_base.h"
+
+namespace openmini {
+namespace synthesizer {
+
+Filter::Filter()
+  : filter_(filters::CreateFilter()),
+    frequency_(0.0f),
+    qfactor_(0.0f),
+    update_(false) {
+  ASSERT(filter_ != nullptr);
+}
+
+Filter::~Filter() {
+  ASSERT(filter_ != nullptr);
+}
+
+void Filter::SetFrequency(const float frequency) {
+  ASSERT(frequency > 0.0f);
+  ASSERT(frequency < openmini::kSamplingRateHalf);
+
+  // TODO: find a way to do this generically
+  if (frequency != frequency_) {
+    frequency_ = frequency;
+    update_ = true;
+  }
+}
+
+void Filter::SetQFactor(const float qfactor) {
+  ASSERT(qfactor <= 100.0f);
+  ASSERT(qfactor >= 0.0f);
+
+  // TODO: find a way to do this generically
+  if (qfactor != qfactor_) {
+    qfactor_ = qfactor;
+    update_ = true;
+  }
+}
+
+Sample Filter::operator()(SampleRead sample) {
+  ASSERT(filter_ != nullptr);
+  ProcessParameters();
+  return (*filter_)(sample);
+}
+
+void Filter::ProcessParameters(void) {
+  ASSERT(filter_ != nullptr);
+  if (update_) {
+    filter_->SetParameters(frequency_, qfactor_);
+    update_ = false;
+  }
+}
+
+}  // namespace synthesizer
+}  // namespace openmini
---
+++ openmini/src/synthesizer/filter.h
@@ -0,0 +1,84 @@
+/// @filename filter.h
+/// @brief Filter: wrapper around an internal filter - definitions
+///
+/// @author gm
+/// @copyright gm 2013
+///
+/// This file is part of OpenMini
+///
+/// OpenMini is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// OpenMini is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef OPENMINI_SRC_SYNTHESIZER_FILTER_H_
+#define OPENMINI_SRC_SYNTHESIZER_FILTER_H_
+
+#include "openmini/src/common.h"
+#include "openmini/src/maths.h"
+
+// Internal forward declarations
+namespace openmini {
+namespace filters {
+class Filter_Base;
+}  // namespace generators
+}  // namespace openmini
+
+namespace openmini {
+namespace synthesizer {
+
+/// @brief Filter: wraps an internal filter and gives it
+/// additional parameters as well as a more advanced parameters management.
+///
+/// It handles everything about asynchronous parameters update.
+class Filter {
+ public:
+  /// @brief Default constructor
+  Filter();
+  /// @brief Default destructor
+  ~Filter();
+  /// @brief Set the filter to the given frequency
+  ///
+  /// Frequency is not normalized here - the unit is Hz
+  ///
+  /// @param[in]    frequency         Frequency to set the filter to
+  void SetFrequency(const float frequency);
+  /// @brief Set the filter to the given q factor
+  ///
+  /// This is not normalized, Q factor within [0.0f ; 100.0f]
+  ///
+  /// @param[in]    qfactor         Q factor to set the filter to
+  void SetQFactor(const float qfactor);
+  /// @brief Actual process function for one sample
+  Sample operator()(SampleRead sample);
+  /// @brief Update internal generator parameters
+  ///
+  /// Allows asynchronous updates; to be called within an update loop.
+  void ProcessParameters(void);
+
+ private:
+  // No assignment operator for this class
+  Filter& operator=(const Filter& right);
+
+  filters::Filter_Base* filter_;  ///< Internal filter
+  float frequency_; ///< Frequency of the filter (non-normalized, in Hz).
+                    /// (due to asynchronous update,
+                    ///< it may as well be the frequency to be applied soon)
+  float qfactor_; ///< Q factor of the filter
+                  ///< Same as above.
+  bool update_;  ///< True if any parameter was updated since the last call to
+                 ///< ProcessParameters()
+};
+
+}  // namespace synthesizer
+}  // namespace openmini
+
+#endif  // OPENMINI_SRC_SYNTHESIZER_FILTER_H_
