07b122f5d0786678f09b677efc7a8f115859d49b
Gama <misept.dieseneuf@gmail.com>
2013-11-27 15:27:09 +0100

    TESTS: Added a debug utility file
    To be included and used for debugging purpose only!
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ openmini/tests/debug.h
@@ -0,0 +1,62 @@
+/// @filename debug.h
+/// @brief Tests common debug include file
+/// @author gm
+/// @copyright gm 2013
+///
+/// This file is part of OpenMini
+///
+/// OpenMini is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// OpenMini is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef OPENMINI_TESTS_DEBUG_H_
+#define OPENMINI_TESTS_DEBUG_H_
+
+#include <fstream>
+#include <string>
+
+#include "openmini/tests/tests.h"
+
+/// @brief Helper class for writing signal data into files
+///
+/// This is clearly not optimized at all nor really clean,
+/// for debugging purpose only!
+class FileWriter {
+ public:
+  FileWriter(const std::string& filename)
+    : out_stream_(filename) {
+    // Nothing to do here for now
+  }
+  ~FileWriter() {
+    out_stream_.close();
+  }
+
+  /// @brief Push one sample into file writer, will be unfolded if
+  /// wrapping a vectorized type
+  void Push(const Sample sample) {
+    // Using the slower runtime version of GetByIndex() for genericity's sake
+    for (unsigned int index(0); index < openmini::SampleSize; index += 1) {
+      out_stream_ << GetByIndex(sample, index) << "\n";
+    }
+  }
+
+  /// @brief Overload for one actual value
+  void Push(const float sample) {
+    out_stream_ << sample << "\n";
+  }
+
+ private:
+  std::ofstream out_stream_;
+};
+
+
+#endif  // OPENMINI_TESTS_DEBUGS_H_
