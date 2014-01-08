17cd9e14ebf56719416354fa549113dbb53cac22
Gama <misept.dieseneuf@gmail.com>
2014-01-08 12:36:24 +0100

    TESTS: Added two new Adapter tests
    - Resizing check (this test should only pass without failing).
    - Check interpolating from 96 to 48 kHz, with random output block sizes.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- openmini/tests/synthesizer/adapter.cc
+++ openmini/tests/synthesizer/adapter.cc
@@ -64,3 +64,96 @@ TEST(Adapter, Passthrough) {
   }
 }
 
+/// @brief When trying to output anything pushed at one sample frequency
+/// into a different output format, we have to check if resizing of the internal
+/// temporary buffer size is properly done
+TEST(Adapter, Resizing) {
+  const float kSamplingFreqIn(96000.0f);
+  const float kSamplingFreqOut(48000.0f);
+  const float kRatio(kSamplingFreqIn / kSamplingFreqOut);
+  const unsigned int kExpectedLength(ExpectedOutLength(kDataTestSetSize,
+                                                       kRatio));
+  std::vector<float> data(kDataTestSetSize);
+  std::vector<float> data_out(kExpectedLength);
+  std::generate(data.begin(),
+                data.end(),
+                std::bind(kNormDistribution, kRandomGenerator));
+
+  Adapter adapter;
+  adapter.SetFrequencies(kSamplingFreqIn, kSamplingFreqOut);
+  adapter.ResizeIfNeedBe(kDataTestSetSize);
+
+  unsigned int data_index(0);
+  // First, we push data in
+  while (data_index < data.size()) {
+    const Sample current(Fill(&data[data_index]));
+    adapter.Push(current);
+    data_index += openmini::SampleSize;
+  }
+
+  data_index = 0;
+  // TODO(gm): Fix this end-of-buffer mess
+  while (data_index < data_out.size() - 32) {
+    std::uniform_int_distribution<int> kBlockSizeDistribution(2,
+                                                              adapter.size());
+    const unsigned int kBlockSize(kBlockSizeDistribution(kRandomGenerator));
+    adapter.Pop(&data_out[data_index], kBlockSize);
+    data_index += kBlockSize;
+  }
+
+  // No checks done:
+  // this test should only not throw for out-of-range array access!
+  EXPECT_TRUE(true);
+}
+
+/// @brief When trying to output a 1kHz sinusoid, whatever the block size,
+/// generated at 96kHz into a 48kHz output one should give us the same signal
+/// (+ noise) than one 1kHz sinusoid directly generated at 48kHz
+TEST(Adapter, 96kTo48kSinusoid) {
+  const float kFrequency(1000.0f);
+  const float kSamplingFreqIn(96000.0f);
+  const float kSamplingFreqOut(48000.0f);
+  const float kRatio(kSamplingFreqIn / kSamplingFreqOut);
+  const unsigned int kExpectedLength(ExpectedOutLength(kDataTestSetSize,
+                                                       kRatio));
+  // Creating sinusoid data
+  std::vector<float> data(kDataTestSetSize);
+  std::vector<float> data_expected(kExpectedLength);
+  std::vector<float> data_out(kExpectedLength);
+  std::generate(data.begin(),
+                data.end(),
+                SinusGenerator(kFrequency, kSamplingFreqIn));
+  std::generate(data_expected.begin(),
+                data_expected.end(),
+                SinusGenerator(kFrequency, kSamplingFreqOut));
+
+  Adapter adapter;
+  adapter.SetFrequencies(kSamplingFreqIn, kSamplingFreqOut);
+
+  adapter.ResizeIfNeedBe(kExpectedLength);
+  unsigned int data_index(0);
+
+  while (data_index < data.size()) {
+    const Sample current(Fill(&data[data_index]));
+    adapter.Push(current);
+    data_index += openmini::SampleSize;
+  }
+
+  data_index = 0;
+  // TODO(gm): Fix this end-of-buffer mess
+  while (data_index < data_out.size() - 32) {
+    std::uniform_int_distribution<int> kBlockSizeDistribution(1,
+                                                              adapter.size());
+    const unsigned int kBlockSize(kBlockSizeDistribution(kRandomGenerator));
+    adapter.Pop(&data_out[data_index], kBlockSize);
+    data_index += kBlockSize;
+  }
+
+  // Data integrity check
+  // a minor epsilon is required due to noise introduced by the interpolation
+  const float kEpsilon(1e-6f);
+  // TODO(gm): Fix this end-of-buffer mess
+  for (unsigned int i(0); i < data_out.size() - 32; ++i) {
+    EXPECT_NEAR(data_expected[i], data_out[i], kEpsilon);
+  }
+}
