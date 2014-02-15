3e3e2b98f0c5c11a77a612bcf3bb7bbada08dded
Gama <misept.dieseneuf@gmail.com>
2014-02-15 18:01:28 +0100

    MODULATORS: Added a C++ implementation of an ADSD modulator
    Based on the one prototyped in Python.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ openmini/src/modulators/adsd.cc
@@ -0,0 +1,122 @@
+/// @filename adsd.cc
+/// @brief Envelop generator using Attack-Decay-Sustain-Decay (ADSD) model
+/// @author gm
+/// @copyright gm 2013
+///
+/// This file is part of OpenMini
+///
+/// OpenMini is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// OpenMini is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "openmini/src/maths.h"
+
+#include "openmini/src/modulators/adsd.h"
+
+namespace openmini {
+namespace modulators {
+
+/// @brief Default max level for the apogee of the attack
+static const float kMaxAmplitude(1.0f);
+
+Adsd::Adsd()
+    : current_increment_(0.0),
+      current_section_(kZero),
+      current_value_(0.0f),
+      sustain_level_(0.0f),
+      cursor_(0),
+      attack_(0),
+      decay_(0),
+      release_(0),
+      actual_decay_(0),
+      actual_release_(0) {
+  // Nothing to do here for now
+}
+Adsd::~Adsd() {
+  // Nothing to do here for now
+}
+
+void Adsd::TriggerOn(void) {
+  cursor_ = 0;
+  current_section_ = kAttack;
+  current_increment_ = ComputeIncrement(kMaxAmplitude, attack_);
+}
+
+void Adsd::TriggerOff(void) {
+  current_section_ = kRelease;
+  current_increment_ = ComputeIncrement(-current_value_, decay_);
+  actual_release_ = cursor_ + decay_;
+}
+
+float Adsd::operator()(void) {
+  switch (current_section_) {
+    case(kAttack): {
+      current_value_ += static_cast<float>(current_increment_);
+      if (cursor_ > attack_) {
+        current_section_ = GetNextSection(current_section_);
+        current_increment_ = ComputeIncrement(sustain_level_ - current_value_,
+                                              decay_);
+      }
+      break;
+    }
+    case(kDecay): {
+      current_value_ += static_cast<float>(current_increment_);
+      if (cursor_ > actual_decay_) {
+        current_section_ = GetNextSection(current_section_);
+        current_value_ = sustain_level_;
+      }
+      break;
+    }
+    case(kSustain): {
+      // Nothing to do here
+      break;
+    }
+    case(kRelease): {
+      if (cursor_ >= actual_release_) {
+        current_section_ = GetNextSection(current_section_);
+      } else {
+        current_value_ += static_cast<float>(current_increment_);
+      }
+      break;
+    }
+    case(kZero): {
+      return 0.0f;
+    }
+    default: {
+      // Should never happen
+      ASSERT(false);
+    }
+  }  // switch(current_section_)
+
+  cursor_ += 1;
+
+  return current_value_;
+}
+
+void Adsd::SetParameters(const unsigned int attack,
+                         const unsigned int decay,
+                         const unsigned int release,
+                         const float sustain_level) {
+  IGNORE(release);
+  attack_ = attack;
+  decay_ = decay;
+  actual_decay_ = decay + attack;
+  sustain_level_ = sustain_level;
+  current_section_ = kZero;
+}
+
+double Adsd::ComputeIncrement(const float rise, const unsigned int run) {
+  return static_cast<double>(rise) / run;
+}
+
+}  // namespace modulators
+}  // namespace openmini
---
+++ openmini/src/modulators/adsd.h
@@ -0,0 +1,73 @@
+/// @filename adsd.h
+/// @brief Envelop generator using Attack-Decay-Sustain-Decay (ADSD) model
+/// @author gm
+/// @copyright gm 2013
+///
+/// This file is part of OpenMini
+///
+/// OpenMini is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// OpenMini is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef OPENMINI_SRC_MODULATORS_ADSD_H_
+#define OPENMINI_SRC_MODULATORS_ADSD_H_
+
+#include <array>
+
+#include "openmini/src/common.h"
+#include "openmini/src/modulators/envelopgenerator_base.h"
+#include "openmini/src/modulators/modulators_common.h"
+
+namespace openmini {
+namespace modulators {
+
+/// @brief 2nd order low pass filter
+/// using the most simple (and computationally efficient) implementation
+class ALIGN Adsd : public EnvelopGenerator_Base {
+ public:
+  Adsd();
+  virtual ~Adsd();
+
+  virtual void TriggerOn(void);
+
+  virtual void TriggerOff(void);
+
+  virtual float operator()(void);
+
+  /// Note that the release here is not used since this is a ADSD:
+  /// the decay setting also sets the release
+  virtual void SetParameters(const unsigned int attack,
+                             const unsigned int decay,
+                             const unsigned int release,
+                             const float sustain_level);
+
+ private:
+  /// @brief Helper function for computing the increment at each increment,
+  /// given the rise (vertical change) and run (horizontal change)
+  double ComputeIncrement(const float rise, const unsigned int run);
+
+  double current_increment_;  ///< Increment to use for the current slope
+  Section current_section_;  ///< The current part of the generated envelop
+  float current_value_;  ///< Current amplitude
+  float sustain_level_;  ///< Amplitude to maintain while sustain is on
+  unsigned int cursor_;  ///< Time cursor (e.g. samples since the last event)
+  unsigned int attack_;  ///< Time setting for the attack
+  unsigned int decay_;  ///< Time setting for the decay
+  unsigned int release_;  ///< Time setting for the attack
+  unsigned int actual_decay_;  ///< Actual decay time in local frame
+  unsigned int actual_release_;  ///<  Actual release time in local frame
+};
+
+}  // namespace modulators
+}  // namespace openmini
+
+#endif  // OPENMINI_SRC_MODULATORS_ADSD_H_
