94000a751633c6b26c4df3a27f5b4e87a993bc99
Gama <misept.dieseneuf@gmail.com>
2014-02-15 15:46:43 +0100

    SCRIPTS: Although still not finished, this ADSD implementation is better
    The previous one was mistaken sustain leel with a time settings, making
    TriggerOff method useless.
    This is better, although still not retriggerable. See #54.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- scripts/modulator_adsd.py
+++ scripts/modulator_adsd.py
@@ -33,21 +33,22 @@ class ADSD(EnvelopGeneratorInterface):
     Implements a attack-decay-sustain-decay envelop generator, mimicking
     the one of the Minimoog
     '''
-    kAmplitudes = [1.0, 0.5, 0.5, 0.0]
+    kMaxAmplitude = 1.0
     def __init__(self, sampling_rate):
         super(ADSD, self).__init__(sampling_rate)
         self._cursor = 0
         self._current_increment = 0.0
         self._current_value = 0.0
         self._time_settings = [0, 0, 0, 0]
+        self._amplitude_settings = [self.kMaxAmplitude, 0, 0, 0]
         self._section = 0
 
-    def SetParameters(self, attack, decay, sustain, release):
+    def SetParameters(self, attack, decay, sustain_level, release):
         self._time_settings[0] = attack
         self._time_settings[1] = decay + self._time_settings[0]
-        self._time_settings[2] = sustain + self._time_settings[1]
-        self._time_settings[3] = decay + self._time_settings[2]
-        rise = self.kAmplitudes[0] - self._current_value
+        self._time_settings[2] = decay
+        self._amplitude_settings = [self.kMaxAmplitude, sustain_level, sustain, 0]
+        rise = self._amplitude_settings[0] - self._current_value
         self._current_increment = self._ComputeIncrement(rise, attack)
         self._section = 4
 
@@ -57,17 +58,31 @@ class ADSD(EnvelopGeneratorInterface):
 
     def TriggerOff(self):
         self._section = 3
+        rise = -self._current_value
+        run = self._time_settings[2]
+        self._current_increment = self._ComputeIncrement(rise, run)
+        self._time_settings[2] += self._cursor
 
     def ProcessSample(self):
         if self._section == 0:
             self._current_value += self._current_increment
+            if (self._cursor > self._time_settings[0]):
+                self._section += 1
+                rise = self._amplitude_settings[self._section] - self._current_value
+                run = self._time_settings[self._section] - self._time_settings[self._section - 1]
+                self._current_increment = self._ComputeIncrement(rise, run)
         elif self._section == 1:
             self._current_value += self._current_increment
+            if (self._cursor > self._time_settings[1]):
+                self._section += 1
         elif self._section == 2:
             # Sustain case
             pass
         elif self._section == 3:
-            self._current_value += self._current_increment
+            if (self._cursor >= self._time_settings[2]):
+                self._section += 1
+            else:
+                self._current_value += self._current_increment
         elif self._section == 4:
             # Out of the envelop case
             return 0.0
@@ -75,16 +90,6 @@ class ADSD(EnvelopGeneratorInterface):
             raise Exception("This should not happen!")
 
         self._cursor += 1
-        if (self._cursor >= self._time_settings[self._section]):
-            self._section += 1
-            if self._section == 4:
-                return 0.0
-            if self._section == 2:
-                rise = 0.0
-            else:
-                rise = self.kAmplitudes[self._section] - self._current_value
-            run = self._time_settings[self._section] - self._time_settings[self._section - 1]
-            self._current_increment = self._ComputeIncrement(rise, run)
 
         return self._current_value
 
@@ -106,11 +111,13 @@ if __name__ == "__main__":
 
     attack = 100
     decay = 50
-    sustain = 30
+    sustain = 0.5
     release = 25
 
-    TriggerOccurence = 384
-    length = 512
+    TriggerOccurence = 512
+    TriggerLength = 384
+
+    length = 1024
 
     generator = ADSD(sampling_freq)
     generator.SetParameters(attack, decay, sustain, release)
@@ -118,6 +125,8 @@ if __name__ == "__main__":
     for idx, _ in enumerate(generated_data):
         if (idx % TriggerOccurence) == 0:
             generator.TriggerOn()
+        if (generator._section == 2) and (idx % TriggerLength) == 0:
+            generator.TriggerOff()
         generated_data[idx] = generator.ProcessSample()
 
     pylab.plot(generated_data)
