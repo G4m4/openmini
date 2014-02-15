4c386917efb26f5e4fad4f66203b665569eed12f
Gama <misept.dieseneuf@gmail.com>
2014-02-15 18:02:27 +0100

    SYNTH: Added a "voltage-controlled amplifier" object
    By analogy with VCF/VCO objects, this wraps an internal envelop generator,
    and allow paramters management as well as appliyng the final envelop on
    any given signal.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

---
+++ openmini/src/synthesizer/vca.cc
@@ -0,0 +1,95 @@
+/// @filename vca.cc
+/// @brief "VCA": Wrapper around a basic envelop generator - implementation
+/// @author gm
+/// @copyright gm 2013
+///
+/// This file is part of OpenMini
+///
+/// OpenMini is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// OpenMini is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "openmini/src/synthesizer/vca.h"
+
+#include "openmini/src/modulators/envelopgenerator_base.h"
+#include "openmini/src/modulators/factory.h"
+
+namespace openmini {
+namespace synthesizer {
+
+Vca::Vca()
+  : // Default on Triangle
+    generator_(modulators::CreateEnvelopGenerator()),
+    attack_(0),
+    decay_(0),
+    release_(0),
+    sustain_level_(0.0f),
+    update_(false) {
+  ASSERT(generator_ != nullptr);
+}
+
+Vca::~Vca() {
+  ASSERT(generator_ != nullptr);
+  modulators::DestroyEnvelopGenerator(generator_);
+}
+
+void Vca::SetAttack(const unsigned int attack) {
+  if (attack != attack_) {
+    attack_ = attack;
+    update_ = true;
+  }
+}
+
+void Vca::SetDecay(const unsigned int decay) {
+  if (decay != decay_) {
+    decay_ = decay;
+    update_ = true;
+  }
+}
+
+void Vca::SetRelease(const unsigned int release) {
+  if (release != release_) {
+    release_ = release;
+    update_ = true;
+  }
+}
+
+void Vca::SetSustain(const float sustain_level) {
+  ASSERT(sustain_level <= 1.0f);
+  ASSERT(sustain_level >= 0.0f);
+
+  if (sustain_level != sustain_level_) {
+    sustain_level_ = sustain_level;
+    update_ = true;
+  }
+}
+
+Sample Vca::operator()(SampleRead input) {
+  ASSERT(generator_ != nullptr);
+  ProcessParameters();
+  const float envelop[4] = {generator_->operator()(),
+                            generator_->operator()(),
+                            generator_->operator()(),
+                            generator_->operator()()};
+  return openmini::Mul(input, Fill(&envelop[0]));
+}
+
+void Vca::ProcessParameters(void) {
+  ASSERT(generator_ != nullptr);
+  if (update_) {
+    generator_->SetParameters(attack_, decay_, release_, sustain_level_);
+    update_ = false;
+  }
+}
+
+}  // namespace synthesizer
+}  // namespace openmini
---
+++ openmini/src/synthesizer/vca.h
@@ -0,0 +1,100 @@
+/// @filename vca.h
+/// @brief "VCA", voltage-controlled amplifier
+/// Wrapper around a basic envelop generator - definitions
+///
+/// @author gm
+/// @copyright gm 2013
+///
+/// This file is part of OpenMini
+///
+/// OpenMini is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// OpenMini is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with OpenMini.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef OPENMINI_SRC_SYNTHESIZER_VCA_H_
+#define OPENMINI_SRC_SYNTHESIZER_VCA_H_
+
+#include "openmini/src/common.h"
+#include "openmini/src/maths.h"
+
+// Internal forward declarations
+namespace openmini {
+namespace modulators {
+class EnvelopGenerator_Base;
+}  // namespace modulators
+}  // namespace openmini
+
+namespace openmini {
+namespace synthesizer {
+
+/// @brief "Voltage controlled amplifier": wraps an envelop generator,
+/// and allow to apply the envelop as a modulation to an input signal.
+///
+/// It handles everything about asynchronous parameters update.
+class Vca {
+ public:
+  /// @brief Default constructor
+  Vca();
+  /// @brief Default destructor
+  ~Vca();
+  /// @brief Set the given attack time
+  ///
+  /// The parameter is not normalized here - the unit is "samples"
+  ///
+  /// @param[in]  attack    Attack time for the envelop
+  void SetAttack(const unsigned int attack);
+  /// @brief Set the given decay time
+  ///
+  /// The parameter is not normalized here - the unit is "samples"
+  ///
+  /// @param[in]  decay    Decay time for the envelop
+  void SetDecay(const unsigned int decay);
+  /// @brief Set the given release time
+  ///
+  /// The parameter is not normalized here - the unit is "samples"
+  ///
+  /// @param[in]  attack    Release time for the envelop
+  void SetRelease(const unsigned int release);
+  /// @brief Set the given sustain level
+  ///
+  /// The parameter is normalized here, supposed to be within [0.0f ; 1.0f]
+  ///
+  /// @param[in]  sustain_level   Level when sustaining
+  void SetSustain(const float sustain_level);
+  /// @brief Actual process function for one sample
+  Sample operator()(SampleRead input);
+  /// @brief Update internal generator parameters
+  ///
+  /// Allows asynchronous updates; to be called within an update loop.
+  void ProcessParameters(void);
+
+ private:
+  // No assignment operator for this class
+  Vca& operator=(const Vca& right);
+
+  modulators::EnvelopGenerator_Base* generator_;  ///< Envelop generator
+  unsigned int attack_; ///< Envelop attack time (due to asynchronous update,
+                        ///< it may as well be the value to be applied soon
+  unsigned int decay_; ///< Envelop decay time (due to asynchronous update,
+                        ///< Same as above.
+  unsigned int release_; ///< Envelop release time (due to asynchronous update,
+                        ///< Same as above.
+  float sustain_level_; ///< Envelop sustain level (normalized).
+                        ///< Same as above.
+  bool update_;  ///< True if any parameter was updated since the last call to
+                 ///< ProcessParameters()
+};
+
+}  // namespace synthesizer
+}  // namespace openmini
+
+#endif  // OPENMINI_SRC_SYNTHESIZER_VCA_H_
