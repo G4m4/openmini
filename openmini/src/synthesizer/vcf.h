ff5046dee044b456b62a314f193b0645eb708111
Gama <misept.dieseneuf@gmail.com>
2014-03-31 14:32:46 +0200

    VCF: Add filter contour feature to the VCF
    This is part of #74.
    
    Signed-off-by: Gama <misept.dieseneuf@gmail.com>

--- openmini/src/synthesizer/vcf.h
+++ openmini/src/synthesizer/vcf.h
@@ -25,6 +25,8 @@
 #include "openmini/src/common.h"
 #include "openmini/src/maths.h"
 
+#include "soundtailor/src/modulators/adsd.h"
+
 // SoundTailor forward declarations
 namespace soundtailor {
 namespace filters {
@@ -45,6 +47,13 @@ class Vcf {
   Vcf();
   /// @brief Default destructor
   ~Vcf();
+
+  /// @brief Event for triggering the beginning of the contour envelop
+  void TriggerOn(void);
+
+  /// @brief Event for triggering the end of the contour envelop
+  void TriggerOff(void);
+
   /// @brief Set the filter to the given frequency
   ///
   /// Frequency is not normalized here - the unit is Hz
@@ -57,8 +66,36 @@ class Vcf {
   ///
   /// @param[in]  resonance   Resonance to set the filter to
   void SetResonance(const float resonance);
+  /// @brief Set the given attack time
   ///
+  /// The parameter is not normalized here - the unit is "samples"
   ///
+  /// @param[in]  attack    Attack time for the envelop
+  void SetAttack(const unsigned int attack);
+  /// @brief Set the given decay time
+  ///
+  /// The parameter is not normalized here - the unit is "samples"
+  ///
+  /// @param[in]  decay    Decay time for the envelop
+  void SetDecay(const unsigned int decay);
+  /// @brief Set the given release time
+  ///
+  /// The parameter is not normalized here - the unit is "samples"
+  ///
+  /// @param[in]  attack    Release time for the envelop
+  void SetRelease(const unsigned int release);
+  /// @brief Set the given sustain level
+  ///
+  /// The parameter is normalized here, supposed to be within [0.0f ; 1.0f]
+  ///
+  /// @param[in]  sustain_level   Level when sustaining
+  void SetSustain(const float sustain_level);
+  /// @brief Set the amount, e.g. Dry/Wet setting
+  ///
+  /// The parameter is normalized here, supposed to be within [0.0f ; 1.0f]
+  ///
+  /// @param[in]  amount   Amount of filter contour: 0.0 -> null
+  void SetAmount(const float amount);
   /// @brief Actual process function for one sample
   Sample operator()(SampleRead sample);
   /// @brief Update internal generator parameters
@@ -67,16 +104,32 @@ class Vcf {
   void ProcessParameters(void);
 
  private:
+  /// @brief Internal helper wrapper for filter contour computation
+  ///
+  /// @return the new filter contour (e.g. its new cutoff frequency)
+  float ComputeContour(void);
   // No assignment operator for this class
   Vcf& operator=(const Vcf& right);
 
   // TODO(gm): polymorphism probably no longer useful here
-  soundtailor::filters::Filter_Base* filter_;  ///< Internal filter
+  soundtailor::filters::Filter_Base* dry_filter_;  ///< Internal dry filter
+  soundtailor::filters::Filter_Base* wet_filter_;  ///< Internal wet filter
+  soundtailor::modulators::Adsd contour_gen_;  ///< Internal envelop generator
+
+  unsigned int attack_; ///< Envelop attack time (due to asynchronous update,
+                        ///< it may as well be the value to be applied soon
+  unsigned int decay_; ///< Envelop decay time (due to asynchronous update,
+                        ///< Same as above.
+  unsigned int release_; ///< Envelop release time (due to asynchronous update,
+                        ///< Same as above.
+  float sustain_level_; ///< Envelop sustain level (normalized).
+                        ///< Same as above.
   float frequency_; ///< Frequency of the filter (non-normalized, in Hz).
                     /// (due to asynchronous update,
                     ///< it may as well be the frequency to be applied soon)
   float resonance_; ///< Resonance of the filter
                   ///< Same as above.
+  float amount_; ///< Dry/Wet tuning
   bool update_;  ///< True if any parameter was updated since the last call to
                  ///< ProcessParameters()
 };
